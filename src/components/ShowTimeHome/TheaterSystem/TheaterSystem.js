8b7e9acd986f86caa3ac915b51652c9d291c2b88
huynhdung295 <huynhdung295>
2021-01-15 12:58:14 +0700

    update search, login, color,...

--- src/components/ShowTimeHome/TheaterSystem/TheaterSystem.js
+++ src/components/ShowTimeHome/TheaterSystem/TheaterSystem.js
@@ -2,10 +2,11 @@ import React, { Fragment } from "react";
 export default function TheaterSystem(props) {
   let { heThongRap } = props;
   const renderHeThongRap = () => {
+
     return heThongRap?.map((rap, index) => {
       return (
         <a
-          className="nav-link"
+          className="nav-link" 
           id="v-pills-home-tab"
           data-toggle="pill"
           href={`#${rap.maHeThongRap}`}
